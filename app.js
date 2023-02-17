dcbe9e29664b51767232693af2ea15477d48ccdd
unknown <hossainmdnazmul268@gmail.com>
2023-02-18 01:48:15 +0600

    tringle btn addeventlistener added

---
+++ app.js
@@ -0,0 +1,54 @@
+function getInputFieldValue(inputId){
+    const inputFieldValue = document.getElementById(inputId)
+    const inputFieldString = inputFieldValue.value;
+    const inputField = parseFloat(inputFieldString)
+
+    inputFieldValue.value = '';
+    return inputField;
+}
+
+
+function getElementInnerText(elementId){
+    const textElement = document.getElementById('triangle');
+    const TextElementValue = textElement.innerText
+    const element = (TextElementValue)
+    
+    return element;
+}
+
+
+
+// this is for click blog btn-part
+
+document.getElementById('blog-btn').addEventListener('click',function(){
+    window.location.href = "Question-ans.html"
+})
+
+
+// here triangle addeventlistener part.....
+document.getElementById('triangle-btn').addEventListener('click',function(){
+   const triangle = getElementInnerText('triangle')
+   
+   const base = getInputFieldValue('base-input');
+   const height =getInputFieldValue('height-input')
+   const area = 0.5 * base * height
+   
+   const number = 0;
+   const count = number + 1;
+   
+  
+   const container = document.getElementById('table-container')
+   const tr = document.createElement('tr');
+   tr.innerHTML = `
+   <td> ${count} </td>
+   <td> ${triangle} </td>
+   <td> ${area} <span>cm<sup> 2 </sup> </span> </td>
+   <td> <button class="btn btn-primary">Button</button> </td>
+   `;
+
+   container.appendChild(tr);
+   
+})
+
+
+// 
